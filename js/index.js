24486cfce8b4e00f38ac15a0b0f2e99a0bd2cd45
ruihildt <ruihildt@protonmail.com>
2019-05-14 15:35:33 +0200

    Add black & white filter when hovering on an image
    With Gabe refactor proposal commented out

--- js/index.js
+++ js/index.js
@@ -1,5 +1,4 @@
 /* Changing the height of nav when scrolling down more than 230px */
-
 //Storing the queryselection in nav
 const nav = document.querySelector(".main-navigation");
 
@@ -16,3 +15,35 @@ function runOnScroll() {
     }
 };
 
+/* On hover, change image to black and white */
+const images = document.querySelectorAll("img");
+
+images.forEach(image => image.addEventListener("mouseover", grayscale));
+imges.forEach(image => image.addEventListener("mouseout", resetColor));
+
+// Callbacks
+function grayscale (event) {
+    event.target.style.filter = "grayscale(100%)";
+}
+function resetColor(event) {
+    event.target.style.filter = "inherit";
+}
+
+// Cabrejas refactor
+
+/* const images = document.querySelectorAll("img");
+
+images.forEach(image => {
+    image.addEventListener(
+        "mouseover",
+        evt => doFilter(evt, "grayscale(100%)")
+    );
+    image.addEventListener(
+        "mouseout",
+        evt => doFilter(evt, "inherit")
+    );
+});
+
+function doFilter(event, filter) {
+    event.target.style.filter = filter;
+} */
