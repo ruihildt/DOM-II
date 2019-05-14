af79b712c8b1903486d7b9254341e1b561bb25bd
ruihildt <ruihildt@protonmail.com>
2019-05-14 14:13:20 +0200

    Change the height of the nav when scrolling

--- js/index.js
+++ js/index.js
@@ -1,1 +1,18 @@
-// Your code goes here
+/* Changing the height of nav when scrolling down more than 230px */
+
+//Storing the queryselection in nav
+const nav = document.querySelector(".main-navigation");
+
+// Setup listener for 
+window.addEventListener('scroll', runOnScroll);
+
+// Function to run on scroll
+function runOnScroll() {
+    let y = window.pageYOffset;
+    if (y > 230) {
+        nav.classList.add("scrollnav");
+    } else {
+        nav.classList.remove("scrollnav");
+    }
+};
+
